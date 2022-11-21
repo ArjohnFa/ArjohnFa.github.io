8d7087c31a5b09d60d5b70374446293a0f513df1
Ariel John <arjohnfajardo379@gmail.com>
2022-11-21 08:28:27 -0500

    cadeau0.1

---
+++ OneDrive/Documents/!!Projects/!Prog 1/examenformatif_2022-11-21/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace examenformatif_2022_11_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
